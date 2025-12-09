5fca3cc13345e57583e90ef2add4969bd18b3625
KM Adnan Absar <mdadnanjilan1234@gmail.com>
2025-12-09 12:11:29 +0600

    added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroBanner from "./components/Banner/page";
+import Community from "./components/Community";
 
 export default function Home() {
   return (
    <>
    <HeroBanner/>
+   <Community></Community>
    </>
   );
 }
