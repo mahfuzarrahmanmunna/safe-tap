0b7d2b8f162bff67c4d3a3183101afffebb83fda
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-12-18 23:14:46 +0600

    added

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,10 @@ import Community from "./components/Community";
 import SafeTapBanner from "./components/SafeTapBanner/SafeTapBanner";
 import SafeTapSection from "./components/SafeTapSection/SafeTapSection";
 import SafeTapAdvantage from "./components/DrinkPrimeAdvantage/DrinkPrimeAdvantage";
+import Category from "./components/Category";
+import WaterJourney from "./components/WaterJourney";
+import HowItWorks from "./components/HowItWorks";
+import FaqData from "./components/FaqData";
 
 export default function Home() {
   return (
@@ -14,6 +18,10 @@ export default function Home() {
       <Community></Community>
       <SafeTapSection />
       <SafeTapAdvantage />
+      <Category/>
+      <WaterJourney/>
+      <HowItWorks/>
+      <FaqData/>
     </>
   );
 }
